808516617c2d9b8cd1c923bd5318c3ffb2972713
Pushpak77 <pushpakc357@gmail.com>
2026-03-10 15:17:09 +0530

    change to alse

--- app.py
+++ app.py
@@ -14,5 +14,5 @@ if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=linux)
 
     port = int(os.environ.get("PORT", 4000))
-    app.run(debug=True,host='0.0.0.0',port=rdp)
+    app.run(debug=False,host='0.0.0.0',port=rdp)
 
