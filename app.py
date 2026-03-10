559e6a97d7bdf4e953b7233d6bda180952463c14
root <root@ip-172-31-28-89.ec2.internal>
2026-03-10 09:49:32 +0000

    change to aws

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def hello():
 if __name__ == "__main__":
 
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True,host='0.0.0.0',port=linux)
+    app.run(debug=True,host='0.0.0.0',port=aws)
 
     port = int(os.environ.get("PORT", 4000))
     app.run(debug=True,host='0.0.0.0',port=rdp)
